772232269bb27c4b079a7060b6bd798989690dae
Mu5sun <musun.lin@aliyun.com>
2019-01-07 20:40:16 +0800

    header

--- free-about/src/page/course/Course.vue
+++ free-about/src/page/course/Course.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="course">
+    <m-header></m-header>
     这是课程
   </div>
 </template>
 
 <script>
+import MHeader from '@/common/components/header/Header'
+
 export default {
-  
+  components: {
+    MHeader
+  }
 }
 </script>
 
