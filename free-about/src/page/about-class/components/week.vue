b7de6e062c8bb458adb0a6f0c9492678a2fde676
Mu5sun <musun.lin@aliyun.com>
2019-01-10 23:37:09 +0800

    date

--- free-about/src/page/about-class/components/week.vue
+++ free-about/src/page/about-class/components/week.vue
@@ -2,9 +2,9 @@
   <div class="week">
     <div class="time-bar">
       <ul class="wrapper">
-        <li class="point" v-for="index in 7" :key="index">
-          <p class="date">{{date}}</p>
-          <p class="weekday">周{{weekday}}</p>
+        <li class="point" v-for="(item, index) in weekArray" :key="index" :class="{active: isToday(index)}">
+          <p class="date">{{day(index)}}</p>
+          <p v-show="isToday(index)" class="weekday">周{{weekday}}</p>
         </li>
       </ul>
     </div>
@@ -13,6 +13,9 @@
 </template>
 
 <script>
+const AVERAGE = 'average'  //平年
+const LEAP = 'leap' //闰年
+
 export default {
   data () {
     return {
@@ -20,26 +23,98 @@ export default {
     }
   },
   methods: {
-    today () {
-      let day = new Date().getDay()
+    day (index) {
+      // 获取今天周几
+      let today = new Date().getDay()
+      // 获取今年是哪年
+      let year = new Date().getFullYear()
+      // 获取当前哪个月，需要加一
+      let month = new Date().getMonth() + 1
+      // 获取今天的日期
+      let day = new Date().getDate()
+      // 比对今天前面还有
+      let num = index - today
+      // 判断当前月份的天数
+      let days = this._judgeDays(year, month)
+      // 计算时间的日期是在昨天之前还是在明天之后
+      day = day + num
+      // 当num=0 也就是当天
+      if (num === 0) {
+        return this._date(month, day)
+      // 当num > 0 也就是明天之后
+      } else if (num > 0) {
+        // 当大于0   表示时间是大于当前月份的天数的
+        if (day - days > 0) {
+          // 相减得出正确的日期
+          day = day - days
+          // 函数得出正确的年和月，后面需要使用
+          date = this._judgeMonth(year, ++month)
+          year = date.year
+          month = date.month
+        }
+      // 最后就是num < 0 的情况 也就是昨天
+      } else {
+        // 当小于等于0的时候，  表示时间是负数
+        if (day <= 0) {
+          // 函数得出正确的年和月，后面需要使用
+          date = this._judgeMonth(year, --month)
+          year = date.year
+          month = date.month
+          // 函数计算出正确的日期
+          day = this._judgeDay(year, month, day)
+        }
+      }
+      return this._date(month, day)
     },
-    paddingZero (num) {
+    isToday (index) {
+      let today = new Date().getDay()
+      if (today === index) {
+        return true
+      } else {
+        return false
+      }
+    },
+    _paddingZero (num) {
       if (num < 10) {
         return `0${num}`
       } else {
         return num
       }
+    },
+    _date (month, day) {
+      return `${this._paddingZero(month)}.${this._paddingZero(day)}`
+    },
+    _judgeDays (year, month) {
+      let day = new Date(year, month, 0);
+      return day.getDate()
+    },
+    _judgeMonth (year, month) {
+      if (month > 12) {
+        year++
+        month = 1
+        return {
+          year,
+          month
+        }
+      } else if (month <= 0) {
+        year--
+        month = 12
+        return {
+          year,
+          month
+        }
+      } else {
+        return month
+      }
+    },
+    _judgeDay (year, month, day) {
+      return this._judgeDays(year, month) + day
     }
   },
   computed: {
     weekday () {
       let weekday = new Date().getDay()
       return this.weekArray[weekday]
-    },
-    date () {
-      let month = new Date().getMonth() + 1
-      let day = new Date().getDate()
-      return `${this.paddingZero(month)}.${this.paddingZero(day)}`
     }
   }
 }
@@ -55,6 +130,8 @@ export default {
   background-color: $color-text
   .time-bar
     position: absolute
+    box-sizing: border-box
+    padding: 40px 0
     top: 0
     right: 0
     bottom: 0
@@ -65,16 +142,15 @@ export default {
       width: 100%
       height: 100%
       display: flex
-      justify-content: space-around
-      align-items: center    
+      justify-content: space-evenly
+      align-items: center
       &::before
         content: ''
         position: absolute
         left: 0
         right: 0
         height: 2px
-        top: 50%
-        transform: translateY(-50%)
+        top: 24px
         background-color: $color-button-background
       .point
         position: relative
@@ -84,6 +160,7 @@ export default {
         border-radius: 50%
         font-size: $font-size-20
         color: $color-text-l
+        margin-bottom: 22px
         &.active
           width: 110px
           height: 50px
